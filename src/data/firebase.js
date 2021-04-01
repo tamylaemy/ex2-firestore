26a71cb04dd6b4cdb529f016b5c3dee35a930971
Tamyla Ait-Chellouche <tamyla-emy@hotmail.fr>
2021-04-01 11:13:46 -0400

    key_correction

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -4,7 +4,12 @@ import 'firebase/firestore';
 /******* Ex#3 - Étape B ********************************/ 
 // Ajouter votre objet de configuration Firebase
 const firebaseConfig = {
-
+  apiKey: "AIzaSyDUrLa7JpKyHkSi-Sc_iRveVSVfftM6WWw",
+  authDomain: "ex2-produits.firebaseapp.com",
+  projectId: "ex2-produits",
+  storageBucket: "ex2-produits.appspot.com",
+  messagingSenderId: "172754654209",
+  appId: "1:172754654209:web:0aadc3c2086b8ace405c00"
 };
 
 // Initialiser Firebase
